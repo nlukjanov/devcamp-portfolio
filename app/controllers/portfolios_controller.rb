9fc646a7fc86da26b065969a1906cc07e7f5c981
Nikita <nlukjanov@gmail.com>
2017-09-20 07:51:29 +0100

    Added custom layouts for blogs and portfolios

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,20 +1,21 @@
 class PortfoliosController < ApplicationController
+  layout "portfolio"
   def index
     @portfolio_items = Portfolio.all
   end
-  
+
   def angular
     @angular_portfolio_items = Portfolio.angular
   end
-  
+
   def new
     @portfolio_item = Portfolio.new
     3.times { @portfolio_item.technologies.build }
   end
-  
+
   def create
     @portfolio_item = Portfolio.new(portfolio_params)
-  
+
     respond_to do |format|
       if @portfolio_item.save
         format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now live!' }
@@ -41,7 +42,7 @@ class PortfoliosController < ApplicationController
   end
 
   def show
-    @portfolio_item= Portfolio.find(params[:id])    
+    @portfolio_item= Portfolio.find(params[:id])
   end
 
   def destroy
@@ -52,9 +53,9 @@ class PortfoliosController < ApplicationController
     # Redirect
     respond_to do |format|
       format.html { redirect_to portfolios_url, notice: "Your portfolio was deleted" }
-    end  
+    end
   end
-  
+
   private
     def portfolio_params
       params.require(:portfolio).permit(:title,
