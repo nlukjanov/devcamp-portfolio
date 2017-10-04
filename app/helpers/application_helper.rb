d4b56aadbfe43be36a3e1674496efa080c03e1b6
Nikita <nlukjanov@gmail.com>
2017-10-04 23:26:09 +0100

    Refactored alerts to helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -68,4 +68,18 @@ module ApplicationHelper
     "active" if current_page? path
   end
 
+
+
+  def alerts
+    alert = (flash[:alert] || flash[:error] || flash[:notice])
+
+    if alert
+      alert_generator alert
+    end
+  end
+
+  def alert_generator msg
+    js add_gritter(msg, title: "Nikita Portfolio", sticky: false)
+  end
+
 end
