5a977d82ecd7b727e6c04ed42c14315e22db70bb
Nikita Lukjanov <nlukjanov@gmail.com>
2017-11-07 15:19:54 +0000

    updated text and seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,73 +24,73 @@ User.create!(
   roles: "site_admin"
 )
 
-puts "1 Regular user created"
+puts '1 Regular user created'
 
-Topic.create!(title: "Rails")
-Topic.create!(title: "HTML + CSS")
-Topic.create!(title: "Coding Exercises")
+Topic.create!(title: 'Rails')
+Topic.create!(title: 'HTML + CSS')
+Topic.create!(title: 'Coding Exercises')
 
-puts "3 Topics created"
+puts '3 Topics created'
 
 3.times do |blog|
   Blog.create!(
     title: "My Blog Post #{blog}",
-    body: "Just some text in the body to exercise in seeds data creation",
+    body: 'Just some text in the body to exercise seeds data creation',
     topic_id: Topic.last.id
   )
 end
 
-puts "3 blog posts created"
+puts '3 blog posts created'
 
 Skill.create!(
-  title: "Rails",
+  title: 'Rails',
   percent_utilized: 40
 )
 
 Skill.create!(
-  title: "HTML + CSS",
+  title: 'HTML + CSS',
   percent_utilized: 25
 )
 
 Skill.create!(
-  title: "Angular",
+  title: 'Angular',
   percent_utilized: 10
 )
 
 Skill.create!(
-  title: "Javascript",
+  title: 'Javascript',
   percent_utilized: 20
 )
 
-puts "4 skills created"
+puts '4 skills created'
 
-4.times do |portfolio_item|
+2.times do |portfolio_item|
   Portfolio.create!(
     title: "Portfolio title: #{portfolio_item}",
-    subtitle: "Ruby on Rails",
-    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    main_image: "http://placehold.it/600x400",
-    thumb_image: "http://placehold.it/350x200",
+    subtitle: 'Ruby on Rails',
+    body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    main_image: 'http://placehold.it/600x400',
+    thumb_image: 'http://placehold.it/350x200'
   )
 end
 
 1.times do |portfolio_item|
   Portfolio.create!(
     title: "Portfolio title: #{portfolio_item}",
-    subtitle: "Angular",
-    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    main_image: "http://placehold.it/600x400",
-    thumb_image: "http://placehold.it/350x200",
+    subtitle: 'Angular',
+    body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    main_image: 'http://placehold.it/600x400',
+    thumb_image: 'http://placehold.it/350x200'
   )
 end
 
-puts "5 portfolios items created"
+puts '3 portfolios items created'
 
 3.times do |technology|
   Portfolio.last.technologies.create!(
     name: "Technology #{technology}",
     portfolio_id: Portfolio.last.id
-    )
+  )
 end
 
-puts("3 technologies created")
+puts('3 technologies created')
